c832d1341eb34fc2860d3adf830fbe52d77c6963
Subhanshu Bansal <subhanshu20135@iiitd.ac.in>
2024-01-14 16:14:18 +0530

    Initial Commit

---
+++ src/main/java/org/example/graphqlrestapi/GraphQlRestApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.graphqlrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/graphqlrestapi/GraphQlRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.graphqlrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
