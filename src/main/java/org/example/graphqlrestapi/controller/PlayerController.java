25013872a008be58be1e69d0cc5419a5d80f67aa
Subhanshu Bansal <subhanshu20135@iiitd.ac.in>
2024-01-14 19:08:45 +0530

    Added Mutations

--- src/main/java/org/example/graphqlrestapi/controller/PlayerController.java
+++ src/main/java/org/example/graphqlrestapi/controller/PlayerController.java
@@ -1,9 +1,11 @@
 package org.example.graphqlrestapi.controller;
 
 import org.example.graphqlrestapi.model.Player;
+import org.example.graphqlrestapi.model.Team;
 import org.example.graphqlrestapi.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +31,16 @@ public class PlayerController {
     @QueryMapping
     public Optional<Player> findOne(@Argument Integer id) {
         return playerService.findOne(id);
+    }
+
+    @MutationMapping
+    public Player create(@Argument String name, @Argument Team team) {
+        return playerService.create(name, team);
+    }
+
+    @MutationMapping
+    public Player update(@Argument Integer id, @Argument String name, @Argument Team team) {
+        return playerService.update(id, name, team);
     }
 
 }
